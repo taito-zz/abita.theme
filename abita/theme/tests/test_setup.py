5b5411bb1f45abee304381f5c1f153468c97bb34
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-03-08 05:59:16 +0200

    got rid of javascript

--- abita/theme/tests/test_setup.py
+++ abita/theme/tests/test_setup.py
@@ -31,16 +31,47 @@ class TestCase(IntegrationTestCase):
         self.failUnless(
             '++theme++abita.theme/css/style.css' in css_resources)
 
-    def test_js_registry_configured(self):
-        js_resources = set(
-            getToolByName(self.portal, 'portal_javascripts').getResourceIds())
+    # def test_js_registry_configured(self):
+    #     js_resources = set(
+    #         getToolByName(self.portal, 'portal_javascripts').getResourceIds())
 
-        self.failUnless(
-            '++theme++abita.theme/javascript/libs/modernizr.custom.js'
-            in js_resources)
-        self.failUnless(
-            '++theme++abita.theme/javascript/libs/respond.min.js'
-            in js_resources)
+    #     self.failUnless(
+    #         '++theme++abita.theme/javascript/libs/modernizr.custom.js'
+    #         in js_resources)
+    #     self.failUnless(
+    #         '++theme++abita.theme/javascript/libs/respond.min.js'
+    #         in js_resources)
+
+
+    def test_theme__enabled(self):
+        from zope.component import getUtility
+        from plone.registry.interfaces import IRegistry
+        registry = getUtility(IRegistry)
+        from plone.app.theming.interfaces import IThemeSettings
+        settings = registry.forInterface(IThemeSettings)
+        self.assertTrue(settings.enabled)
+
+    def test_theme__rules(self):
+        from zope.component import getUtility
+        from plone.registry.interfaces import IRegistry
+        registry = getUtility(IRegistry)
+        from plone.app.theming.interfaces import IThemeSettings
+        settings = registry.forInterface(IThemeSettings)
+        self.assertEqual(
+            settings.rules,
+            "/++theme++abita.theme/rules.xml"
+        )
+
+    def test_theme__absolutePrefix(self):
+        from zope.component import getUtility
+        from plone.registry.interfaces import IRegistry
+        registry = getUtility(IRegistry)
+        from plone.app.theming.interfaces import IThemeSettings
+        settings = registry.forInterface(IThemeSettings)
+        self.assertEqual(
+            settings.absolutePrefix,
+            "/++theme++abita.theme"
+        )
 
     def test_doctype_configured(self):
         from plone.app.theming.interfaces import IThemeSettings
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='abita.theme',
-    version='0.0',
+    version='0.1',
     description="ABITA Theme",
     long_description=open("README.rst").read(),
     # Get more strings from
