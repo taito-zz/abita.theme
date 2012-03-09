ed3bf8672d97dc92ca21453436abc5dad49557b5
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-03-09 07:32:02 +0200

    version updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='abita.theme',
-    version='0.2',
+    version='0.3',
     description="ABITA Theme",
     long_description=open("README.rst").read(),
     # Get more strings from
