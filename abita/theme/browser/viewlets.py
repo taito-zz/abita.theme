c5fff9ef08ece37b6d3448e419a7e68367a0315e
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-08-22 23:10:50 +0300

    viewlet removed

--- abita/theme/browser/viewlets.py
+++ abita/theme/browser/viewlets.py
@@ -1,6 +1,5 @@
 from Acquisition import aq_inner
 from Products.ATContentTypes.interfaces.document import IATDocument
-from Products.ATContentTypes.interfaces.news import IATNewsItem
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.contentlisting.interfaces import IContentListing
@@ -14,37 +13,35 @@ class FeedViewlet(ViewletBase):
 
     def feeds(
         self,
-        limit=3,
+        limit=0,
         object_provides=IATDocument.__identifier__,
         path=None,
-        Subject=None,
-        ):
+        Subject=None):
         context = aq_inner(self.context)
         catalog = getToolByName(context, 'portal_catalog')
         query = {
             'object_provides': object_provides,
-            'sort_on': 'modified', 
+            'sort_on': 'modified',
             'sort_order': 'reverse',
-            'sort_limit': limit,
         }
         if path is not None:
             query['path'] = path
         if Subject is not None:
             query['Subject'] = Subject
-        items = catalog(query)[:limit]
+        brains = []
+        if limit:
+            query['sort_limit'] = limit
+            brains = catalog(query)[:limit]
+        items = catalog(query) if not brains else brains
         ploneview = getMultiAdapter(
-            (context, self.request),
-            name=u'plone'
-        )
+            (context, self.request), name=u'plone')
         if items:
-            items = [
-                {
-                    'title': item.Title(),
-                    'url': item.getURL(),
-                    'description': self.description(item),
-                    'date': ploneview.toLocalizedTime(item.ModificationDate()),
-                } for item in IContentListing(items)
-            ]
+            items = [{
+                'title': item.Title(),
+                'url': item.getURL(),
+                'description': self.description(item),
+                'date': ploneview.toLocalizedTime(item.ModificationDate()),
+            } for item in IContentListing(items)]
         return items
 
     def description(self, item):
@@ -52,9 +49,7 @@ class FeedViewlet(ViewletBase):
         length = 200
         if len(desc) > length:
             ploneview = getMultiAdapter(
-                (self.context, self.request),
-                name=u'plone'
-            )
+                (self.context, self.request), name=u'plone')
             desc = ploneview.cropText(desc, length)
         return desc
 
@@ -65,32 +60,6 @@ class FeedViewlet(ViewletBase):
     def url(self):
         if self.context.get(self.id):
             return self.context[self.id].absolute_url()
-
-class BusinessTopicsViewlet(FeedViewlet):
-
-    id = 'topics'
-
-    def items(self):
-        portal_state = getMultiAdapter(
-            (self.context, self.request),
-            name=u'plone_portal_state'
-        )
-        path = {
-                'query': '{0}/topics'.format(portal_state.navigation_root_path()),
-                'depth': 1,
-        }
-        return self.feeds(limit=1, path=path)
-
-
-class NewsViewlet(FeedViewlet):
-
-    id = 'news'
-
-    def items(self):
-        return self.feeds(
-            object_provides=IATNewsItem.__identifier__
-        )
-
 
 class ApplicationReleasesViewlet(FeedViewlet):
 
@@ -105,4 +74,4 @@ class ApplicationReleasesViewlet(FeedViewlet):
                 'query': '{0}/applications'.format(portal_state.navigation_root_path()),
                 'depth': 1,
         }
-        return self.feeds(limit=1, path=path)
+        return self.feeds(limit=20, path=path)
