73ba2e5956d10b73b4148ee7bc74f91e5a438729
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-02-19 22:29:34 +0200

    Release 0.8.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='abita.theme',
-    version='0.8',
+    version='0.8.1',
     description="ABITA Theme",
     long_description=open("README.rst").read(),
     classifiers=[
