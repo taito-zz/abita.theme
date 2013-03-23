702741f3bf5ab94962e7f6444058701438a4a105
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-03-23 20:03:50 +0200

    Relase 0.13.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='abita.theme',
-    version='0.13.2',
+    version='0.13.3',
     description="ABITA Theme",
     long_description=open("README.rst").read(),
     classifiers=[
